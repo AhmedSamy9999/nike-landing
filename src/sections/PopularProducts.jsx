6531477ade17015cbb1d7380c0e1769b242aa801
Ahmed Samy <115529140+AhmedSamy9999@users.noreply.github.com>
2023-08-22 19:28:02 +0300

    create popluar product card

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,3 +1,4 @@
+import PopularProductCard from "../components/PopularProductCard";
 import { products } from "../constants";
 
 const PopularProducts = () => {
@@ -11,6 +12,11 @@ const PopularProducts = () => {
           Experience top-notch quality and style with our sought-after
           selections. Discover a world of comfort, design and value
         </p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
       </div>
     </section>
   );
